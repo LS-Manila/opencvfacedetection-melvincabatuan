6c9e327fef560f29708c0f901607b0baffa7705a
melvincabatuan <melvincabatuan@gmail.com>
2015-11-02 13:14:44 +0800

    added OpenCV3-FaceDetection Samle

---
+++ app/obj/local/armeabi-v7a/objs/detection_based_tracker/DetectionBasedTracker_jni.o.d
@@ -0,0 +1,167 @@
+/home/cobalt/AndroidStudioProjects/OpenCV3-FaceDetection/app/obj/local/armeabi-v7a/objs/detection_based_tracker/DetectionBasedTracker_jni.o: \
+ /home/cobalt/AndroidStudioProjects/OpenCV3-FaceDetection/app/jni/DetectionBasedTracker_jni.cpp \
+ /home/cobalt/AndroidStudioProjects/OpenCV3-FaceDetection/app/jni/DetectionBasedTracker_jni.h \
+ /home/cobalt/Android/adt-bundle-linux-x86-20131030/android-ndk-r10d/platforms/android-8/arch-arm/usr/include/jni.h \
+ /home/cobalt/Android/adt-bundle-linux-x86-20131030/android-ndk-r10d/platforms/android-8/arch-arm/usr/include/sys/cdefs.h \
+ /home/cobalt/Android/adt-bundle-linux-x86-20131030/android-ndk-r10d/platforms/android-8/arch-arm/usr/include/sys/cdefs_elf.h \
+ /home/cobalt/Android/adt-bundle-linux-x86-20131030/android-ndk-r10d/platforms/android-8/arch-arm/usr/include/android/api-level.h \
+ /home/cobalt/Android/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/core.hpp \
+ /home/cobalt/Android/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core.hpp \
+ /home/cobalt/Android/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/cvdef.h \
+ /home/cobalt/Android/OpenCV-android-sdk/sdk/native/jni/include/opencv2/hal/defs.h \
+ /home/cobalt/Android/adt-bundle-linux-x86-20131030/android-ndk-r10d/platforms/android-8/arch-arm/usr/include/limits.h \
+ /home/cobalt/Android/adt-bundle-linux-x86-20131030/android-ndk-r10d/platforms/android-8/arch-arm/usr/include/sys/limits.h \
+ /home/cobalt/Android/adt-bundle-linux-x86-20131030/android-ndk-r10d/platforms/android-8/arch-arm/usr/include/linux/limits.h \
+ /home/cobalt/Android/adt-bundle-linux-x86-20131030/android-ndk-r10d/platforms/android-8/arch-arm/usr/include/machine/internal_types.h \
+ /home/cobalt/Android/adt-bundle-linux-x86-20131030/android-ndk-r10d/platforms/android-8/arch-arm/usr/include/machine/limits.h \
+ /home/cobalt/Android/adt-bundle-linux-x86-20131030/android-ndk-r10d/platforms/android-8/arch-arm/usr/include/sys/syslimits.h \
+ /home/cobalt/Android/adt-bundle-linux-x86-20131030/android-ndk-r10d/platforms/android-8/arch-arm/usr/include/asm/page.h \
+ /home/cobalt/Android/adt-bundle-linux-x86-20131030/android-ndk-r10d/platforms/android-8/arch-arm/usr/include/stdint.h \
+ /home/cobalt/Android/adt-bundle-linux-x86-20131030/android-ndk-r10d/platforms/android-8/arch-arm/usr/include/sys/_types.h \
+ /home/cobalt/Android/adt-bundle-linux-x86-20131030/android-ndk-r10d/platforms/android-8/arch-arm/usr/include/machine/_types.h \
+ /home/cobalt/Android/adt-bundle-linux-x86-20131030/android-ndk-r10d/sources/cxx-stl/gnu-libstdc++/4.8/include/cmath \
+ /home/cobalt/Android/adt-bundle-linux-x86-20131030/android-ndk-r10d/sources/cxx-stl/gnu-libstdc++/4.8/include/algorithm \
+ /home/cobalt/Android/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/version.hpp \
+ /home/cobalt/Android/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/base.hpp \
+ /home/cobalt/Android/adt-bundle-linux-x86-20131030/android-ndk-r10d/sources/cxx-stl/gnu-libstdc++/4.8/include/climits \
+ /home/cobalt/Android/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/cvstd.hpp \
+ /home/cobalt/Android/adt-bundle-linux-x86-20131030/android-ndk-r10d/sources/cxx-stl/gnu-libstdc++/4.8/include/cstddef \
+ /home/cobalt/Android/adt-bundle-linux-x86-20131030/android-ndk-r10d/sources/cxx-stl/gnu-libstdc++/4.8/include/cstring \
+ /home/cobalt/Android/adt-bundle-linux-x86-20131030/android-ndk-r10d/sources/cxx-stl/gnu-libstdc++/4.8/include/cctype \
+ /home/cobalt/Android/adt-bundle-linux-x86-20131030/android-ndk-r10d/sources/cxx-stl/gnu-libstdc++/4.8/include/string \
+ /home/cobalt/Android/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/ptr.inl.hpp \
+ /home/cobalt/Android/OpenCV-android-sdk/sdk/native/jni/include/opencv2/hal.hpp \
+ /home/cobalt/Android/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/sse_utils.hpp \
+ /home/cobalt/Android/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/traits.hpp \
+ /home/cobalt/Android/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/matx.hpp \
+ /home/cobalt/Android/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/types.hpp \
+ /home/cobalt/Android/adt-bundle-linux-x86-20131030/android-ndk-r10d/sources/cxx-stl/gnu-libstdc++/4.8/include/cfloat \
+ /home/cobalt/Android/adt-bundle-linux-x86-20131030/android-ndk-r10d/sources/cxx-stl/gnu-libstdc++/4.8/include/vector \
+ /home/cobalt/Android/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/mat.hpp \
+ /home/cobalt/Android/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/bufferpool.hpp \
+ /home/cobalt/Android/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/mat.inl.hpp \
+ /home/cobalt/Android/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/persistence.hpp \
+ /home/cobalt/Android/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/operations.hpp \
+ /home/cobalt/Android/adt-bundle-linux-x86-20131030/android-ndk-r10d/sources/cxx-stl/gnu-libstdc++/4.8/include/cstdio \
+ /home/cobalt/Android/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/cvstd.inl.hpp \
+ /home/cobalt/Android/adt-bundle-linux-x86-20131030/android-ndk-r10d/sources/cxx-stl/gnu-libstdc++/4.8/include/complex \
+ /home/cobalt/Android/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/utility.hpp \
+ /home/cobalt/Android/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/core_c.h \
+ /home/cobalt/Android/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/types_c.h \
+ /home/cobalt/Android/adt-bundle-linux-x86-20131030/android-ndk-r10d/platforms/android-8/arch-arm/usr/include/assert.h \
+ /home/cobalt/Android/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/optim.hpp \
+ /home/cobalt/Android/OpenCV-android-sdk/sdk/native/jni/include/opencv2/objdetect.hpp \
+ /home/cobalt/Android/OpenCV-android-sdk/sdk/native/jni/include/opencv2/objdetect/detection_based_tracker.hpp \
+ /home/cobalt/Android/OpenCV-android-sdk/sdk/native/jni/include/opencv2/objdetect/objdetect_c.h \
+ /home/cobalt/Android/adt-bundle-linux-x86-20131030/android-ndk-r10d/sources/cxx-stl/gnu-libstdc++/4.8/include/deque \
+ /home/cobalt/Android/adt-bundle-linux-x86-20131030/android-ndk-r10d/platforms/android-8/arch-arm/usr/include/android/log.h
+
+/home/cobalt/AndroidStudioProjects/OpenCV3-FaceDetection/app/jni/DetectionBasedTracker_jni.h:
+
+/home/cobalt/Android/adt-bundle-linux-x86-20131030/android-ndk-r10d/platforms/android-8/arch-arm/usr/include/jni.h:
+
+/home/cobalt/Android/adt-bundle-linux-x86-20131030/android-ndk-r10d/platforms/android-8/arch-arm/usr/include/sys/cdefs.h:
+
+/home/cobalt/Android/adt-bundle-linux-x86-20131030/android-ndk-r10d/platforms/android-8/arch-arm/usr/include/sys/cdefs_elf.h:
+
+/home/cobalt/Android/adt-bundle-linux-x86-20131030/android-ndk-r10d/platforms/android-8/arch-arm/usr/include/android/api-level.h:
+
+/home/cobalt/Android/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/core.hpp:
+
+/home/cobalt/Android/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core.hpp:
+
+/home/cobalt/Android/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/cvdef.h:
+
+/home/cobalt/Android/OpenCV-android-sdk/sdk/native/jni/include/opencv2/hal/defs.h:
+
+/home/cobalt/Android/adt-bundle-linux-x86-20131030/android-ndk-r10d/platforms/android-8/arch-arm/usr/include/limits.h:
+
+/home/cobalt/Android/adt-bundle-linux-x86-20131030/android-ndk-r10d/platforms/android-8/arch-arm/usr/include/sys/limits.h:
+
+/home/cobalt/Android/adt-bundle-linux-x86-20131030/android-ndk-r10d/platforms/android-8/arch-arm/usr/include/linux/limits.h:
+
+/home/cobalt/Android/adt-bundle-linux-x86-20131030/android-ndk-r10d/platforms/android-8/arch-arm/usr/include/machine/internal_types.h:
+
+/home/cobalt/Android/adt-bundle-linux-x86-20131030/android-ndk-r10d/platforms/android-8/arch-arm/usr/include/machine/limits.h:
+
+/home/cobalt/Android/adt-bundle-linux-x86-20131030/android-ndk-r10d/platforms/android-8/arch-arm/usr/include/sys/syslimits.h:
+
+/home/cobalt/Android/adt-bundle-linux-x86-20131030/android-ndk-r10d/platforms/android-8/arch-arm/usr/include/asm/page.h:
+
+/home/cobalt/Android/adt-bundle-linux-x86-20131030/android-ndk-r10d/platforms/android-8/arch-arm/usr/include/stdint.h:
+
+/home/cobalt/Android/adt-bundle-linux-x86-20131030/android-ndk-r10d/platforms/android-8/arch-arm/usr/include/sys/_types.h:
+
+/home/cobalt/Android/adt-bundle-linux-x86-20131030/android-ndk-r10d/platforms/android-8/arch-arm/usr/include/machine/_types.h:
+
+/home/cobalt/Android/adt-bundle-linux-x86-20131030/android-ndk-r10d/sources/cxx-stl/gnu-libstdc++/4.8/include/cmath:
+
+/home/cobalt/Android/adt-bundle-linux-x86-20131030/android-ndk-r10d/sources/cxx-stl/gnu-libstdc++/4.8/include/algorithm:
+
+/home/cobalt/Android/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/version.hpp:
+
+/home/cobalt/Android/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/base.hpp:
+
+/home/cobalt/Android/adt-bundle-linux-x86-20131030/android-ndk-r10d/sources/cxx-stl/gnu-libstdc++/4.8/include/climits:
+
+/home/cobalt/Android/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/cvstd.hpp:
+
+/home/cobalt/Android/adt-bundle-linux-x86-20131030/android-ndk-r10d/sources/cxx-stl/gnu-libstdc++/4.8/include/cstddef:
+
+/home/cobalt/Android/adt-bundle-linux-x86-20131030/android-ndk-r10d/sources/cxx-stl/gnu-libstdc++/4.8/include/cstring:
+
+/home/cobalt/Android/adt-bundle-linux-x86-20131030/android-ndk-r10d/sources/cxx-stl/gnu-libstdc++/4.8/include/cctype:
+
+/home/cobalt/Android/adt-bundle-linux-x86-20131030/android-ndk-r10d/sources/cxx-stl/gnu-libstdc++/4.8/include/string:
+
+/home/cobalt/Android/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/ptr.inl.hpp:
+
+/home/cobalt/Android/OpenCV-android-sdk/sdk/native/jni/include/opencv2/hal.hpp:
+
+/home/cobalt/Android/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/sse_utils.hpp:
+
+/home/cobalt/Android/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/traits.hpp:
+
+/home/cobalt/Android/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/matx.hpp:
+
+/home/cobalt/Android/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/types.hpp:
+
+/home/cobalt/Android/adt-bundle-linux-x86-20131030/android-ndk-r10d/sources/cxx-stl/gnu-libstdc++/4.8/include/cfloat:
+
+/home/cobalt/Android/adt-bundle-linux-x86-20131030/android-ndk-r10d/sources/cxx-stl/gnu-libstdc++/4.8/include/vector:
+
+/home/cobalt/Android/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/mat.hpp:
+
+/home/cobalt/Android/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/bufferpool.hpp:
+
+/home/cobalt/Android/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/mat.inl.hpp:
+
+/home/cobalt/Android/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/persistence.hpp:
+
+/home/cobalt/Android/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/operations.hpp:
+
+/home/cobalt/Android/adt-bundle-linux-x86-20131030/android-ndk-r10d/sources/cxx-stl/gnu-libstdc++/4.8/include/cstdio:
+
+/home/cobalt/Android/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/cvstd.inl.hpp:
+
+/home/cobalt/Android/adt-bundle-linux-x86-20131030/android-ndk-r10d/sources/cxx-stl/gnu-libstdc++/4.8/include/complex:
+
+/home/cobalt/Android/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/utility.hpp:
+
+/home/cobalt/Android/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/core_c.h:
+
+/home/cobalt/Android/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/types_c.h:
+
+/home/cobalt/Android/adt-bundle-linux-x86-20131030/android-ndk-r10d/platforms/android-8/arch-arm/usr/include/assert.h:
+
+/home/cobalt/Android/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/optim.hpp:
+
+/home/cobalt/Android/OpenCV-android-sdk/sdk/native/jni/include/opencv2/objdetect.hpp:
+
+/home/cobalt/Android/OpenCV-android-sdk/sdk/native/jni/include/opencv2/objdetect/detection_based_tracker.hpp:
+
+/home/cobalt/Android/OpenCV-android-sdk/sdk/native/jni/include/opencv2/objdetect/objdetect_c.h:
+
+/home/cobalt/Android/adt-bundle-linux-x86-20131030/android-ndk-r10d/sources/cxx-stl/gnu-libstdc++/4.8/include/deque:
+
+/home/cobalt/Android/adt-bundle-linux-x86-20131030/android-ndk-r10d/platforms/android-8/arch-arm/usr/include/android/log.h:
---
+++ app/obj/local/armeabi-v7a/objs/mixed_sample/jni_part.o.d
@@ -0,0 +1,176 @@
+/home/cobalt/AndroidStudioProjects/OpenCV3-MixedProcessing/app/obj/local/armeabi-v7a/objs/mixed_sample/jni_part.o: \
+ /home/cobalt/AndroidStudioProjects/OpenCV3-MixedProcessing/app/jni/jni_part.cpp \
+ /home/cobalt/Android/adt-bundle-linux-x86-20131030/android-ndk-r10d/platforms/android-8/arch-arm/usr/include/jni.h \
+ /home/cobalt/Android/adt-bundle-linux-x86-20131030/android-ndk-r10d/platforms/android-8/arch-arm/usr/include/sys/cdefs.h \
+ /home/cobalt/Android/adt-bundle-linux-x86-20131030/android-ndk-r10d/platforms/android-8/arch-arm/usr/include/sys/cdefs_elf.h \
+ /home/cobalt/Android/adt-bundle-linux-x86-20131030/android-ndk-r10d/platforms/android-8/arch-arm/usr/include/android/api-level.h \
+ /home/cobalt/Android/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/core.hpp \
+ /home/cobalt/Android/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core.hpp \
+ /home/cobalt/Android/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/cvdef.h \
+ /home/cobalt/Android/OpenCV-android-sdk/sdk/native/jni/include/opencv2/hal/defs.h \
+ /home/cobalt/Android/adt-bundle-linux-x86-20131030/android-ndk-r10d/platforms/android-8/arch-arm/usr/include/limits.h \
+ /home/cobalt/Android/adt-bundle-linux-x86-20131030/android-ndk-r10d/platforms/android-8/arch-arm/usr/include/sys/limits.h \
+ /home/cobalt/Android/adt-bundle-linux-x86-20131030/android-ndk-r10d/platforms/android-8/arch-arm/usr/include/linux/limits.h \
+ /home/cobalt/Android/adt-bundle-linux-x86-20131030/android-ndk-r10d/platforms/android-8/arch-arm/usr/include/machine/internal_types.h \
+ /home/cobalt/Android/adt-bundle-linux-x86-20131030/android-ndk-r10d/platforms/android-8/arch-arm/usr/include/machine/limits.h \
+ /home/cobalt/Android/adt-bundle-linux-x86-20131030/android-ndk-r10d/platforms/android-8/arch-arm/usr/include/sys/syslimits.h \
+ /home/cobalt/Android/adt-bundle-linux-x86-20131030/android-ndk-r10d/platforms/android-8/arch-arm/usr/include/asm/page.h \
+ /home/cobalt/Android/adt-bundle-linux-x86-20131030/android-ndk-r10d/platforms/android-8/arch-arm/usr/include/stdint.h \
+ /home/cobalt/Android/adt-bundle-linux-x86-20131030/android-ndk-r10d/platforms/android-8/arch-arm/usr/include/sys/_types.h \
+ /home/cobalt/Android/adt-bundle-linux-x86-20131030/android-ndk-r10d/platforms/android-8/arch-arm/usr/include/machine/_types.h \
+ /home/cobalt/Android/adt-bundle-linux-x86-20131030/android-ndk-r10d/sources/cxx-stl/gnu-libstdc++/4.8/include/cmath \
+ /home/cobalt/Android/adt-bundle-linux-x86-20131030/android-ndk-r10d/sources/cxx-stl/gnu-libstdc++/4.8/include/algorithm \
+ /home/cobalt/Android/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/version.hpp \
+ /home/cobalt/Android/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/base.hpp \
+ /home/cobalt/Android/adt-bundle-linux-x86-20131030/android-ndk-r10d/sources/cxx-stl/gnu-libstdc++/4.8/include/climits \
+ /home/cobalt/Android/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/cvstd.hpp \
+ /home/cobalt/Android/adt-bundle-linux-x86-20131030/android-ndk-r10d/sources/cxx-stl/gnu-libstdc++/4.8/include/cstddef \
+ /home/cobalt/Android/adt-bundle-linux-x86-20131030/android-ndk-r10d/sources/cxx-stl/gnu-libstdc++/4.8/include/cstring \
+ /home/cobalt/Android/adt-bundle-linux-x86-20131030/android-ndk-r10d/sources/cxx-stl/gnu-libstdc++/4.8/include/cctype \
+ /home/cobalt/Android/adt-bundle-linux-x86-20131030/android-ndk-r10d/sources/cxx-stl/gnu-libstdc++/4.8/include/string \
+ /home/cobalt/Android/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/ptr.inl.hpp \
+ /home/cobalt/Android/OpenCV-android-sdk/sdk/native/jni/include/opencv2/hal.hpp \
+ /home/cobalt/Android/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/sse_utils.hpp \
+ /home/cobalt/Android/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/traits.hpp \
+ /home/cobalt/Android/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/matx.hpp \
+ /home/cobalt/Android/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/types.hpp \
+ /home/cobalt/Android/adt-bundle-linux-x86-20131030/android-ndk-r10d/sources/cxx-stl/gnu-libstdc++/4.8/include/cfloat \
+ /home/cobalt/Android/adt-bundle-linux-x86-20131030/android-ndk-r10d/sources/cxx-stl/gnu-libstdc++/4.8/include/vector \
+ /home/cobalt/Android/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/mat.hpp \
+ /home/cobalt/Android/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/bufferpool.hpp \
+ /home/cobalt/Android/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/mat.inl.hpp \
+ /home/cobalt/Android/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/persistence.hpp \
+ /home/cobalt/Android/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/operations.hpp \
+ /home/cobalt/Android/adt-bundle-linux-x86-20131030/android-ndk-r10d/sources/cxx-stl/gnu-libstdc++/4.8/include/cstdio \
+ /home/cobalt/Android/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/cvstd.inl.hpp \
+ /home/cobalt/Android/adt-bundle-linux-x86-20131030/android-ndk-r10d/sources/cxx-stl/gnu-libstdc++/4.8/include/complex \
+ /home/cobalt/Android/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/utility.hpp \
+ /home/cobalt/Android/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/core_c.h \
+ /home/cobalt/Android/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/types_c.h \
+ /home/cobalt/Android/adt-bundle-linux-x86-20131030/android-ndk-r10d/platforms/android-8/arch-arm/usr/include/assert.h \
+ /home/cobalt/Android/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/optim.hpp \
+ /home/cobalt/Android/OpenCV-android-sdk/sdk/native/jni/include/opencv2/imgproc/imgproc.hpp \
+ /home/cobalt/Android/OpenCV-android-sdk/sdk/native/jni/include/opencv2/imgproc.hpp \
+ /home/cobalt/Android/OpenCV-android-sdk/sdk/native/jni/include/opencv2/imgproc/imgproc_c.h \
+ /home/cobalt/Android/OpenCV-android-sdk/sdk/native/jni/include/opencv2/imgproc/types_c.h \
+ /home/cobalt/Android/OpenCV-android-sdk/sdk/native/jni/include/opencv2/features2d/features2d.hpp \
+ /home/cobalt/Android/OpenCV-android-sdk/sdk/native/jni/include/opencv2/features2d.hpp \
+ /home/cobalt/Android/OpenCV-android-sdk/sdk/native/jni/include/opencv2/flann/miniflann.hpp \
+ /home/cobalt/Android/OpenCV-android-sdk/sdk/native/jni/include/opencv2/flann/defines.h \
+ /home/cobalt/Android/OpenCV-android-sdk/sdk/native/jni/include/opencv2/flann/config.h
+
+/home/cobalt/Android/adt-bundle-linux-x86-20131030/android-ndk-r10d/platforms/android-8/arch-arm/usr/include/jni.h:
+
+/home/cobalt/Android/adt-bundle-linux-x86-20131030/android-ndk-r10d/platforms/android-8/arch-arm/usr/include/sys/cdefs.h:
+
+/home/cobalt/Android/adt-bundle-linux-x86-20131030/android-ndk-r10d/platforms/android-8/arch-arm/usr/include/sys/cdefs_elf.h:
+
+/home/cobalt/Android/adt-bundle-linux-x86-20131030/android-ndk-r10d/platforms/android-8/arch-arm/usr/include/android/api-level.h:
+
+/home/cobalt/Android/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/core.hpp:
+
+/home/cobalt/Android/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core.hpp:
+
+/home/cobalt/Android/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/cvdef.h:
+
+/home/cobalt/Android/OpenCV-android-sdk/sdk/native/jni/include/opencv2/hal/defs.h:
+
+/home/cobalt/Android/adt-bundle-linux-x86-20131030/android-ndk-r10d/platforms/android-8/arch-arm/usr/include/limits.h:
+
+/home/cobalt/Android/adt-bundle-linux-x86-20131030/android-ndk-r10d/platforms/android-8/arch-arm/usr/include/sys/limits.h:
+
+/home/cobalt/Android/adt-bundle-linux-x86-20131030/android-ndk-r10d/platforms/android-8/arch-arm/usr/include/linux/limits.h:
+
+/home/cobalt/Android/adt-bundle-linux-x86-20131030/android-ndk-r10d/platforms/android-8/arch-arm/usr/include/machine/internal_types.h:
+
+/home/cobalt/Android/adt-bundle-linux-x86-20131030/android-ndk-r10d/platforms/android-8/arch-arm/usr/include/machine/limits.h:
+
+/home/cobalt/Android/adt-bundle-linux-x86-20131030/android-ndk-r10d/platforms/android-8/arch-arm/usr/include/sys/syslimits.h:
+
+/home/cobalt/Android/adt-bundle-linux-x86-20131030/android-ndk-r10d/platforms/android-8/arch-arm/usr/include/asm/page.h:
+
+/home/cobalt/Android/adt-bundle-linux-x86-20131030/android-ndk-r10d/platforms/android-8/arch-arm/usr/include/stdint.h:
+
+/home/cobalt/Android/adt-bundle-linux-x86-20131030/android-ndk-r10d/platforms/android-8/arch-arm/usr/include/sys/_types.h:
+
+/home/cobalt/Android/adt-bundle-linux-x86-20131030/android-ndk-r10d/platforms/android-8/arch-arm/usr/include/machine/_types.h:
+
+/home/cobalt/Android/adt-bundle-linux-x86-20131030/android-ndk-r10d/sources/cxx-stl/gnu-libstdc++/4.8/include/cmath:
+
+/home/cobalt/Android/adt-bundle-linux-x86-20131030/android-ndk-r10d/sources/cxx-stl/gnu-libstdc++/4.8/include/algorithm:
+
+/home/cobalt/Android/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/version.hpp:
+
+/home/cobalt/Android/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/base.hpp:
+
+/home/cobalt/Android/adt-bundle-linux-x86-20131030/android-ndk-r10d/sources/cxx-stl/gnu-libstdc++/4.8/include/climits:
+
+/home/cobalt/Android/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/cvstd.hpp:
+
+/home/cobalt/Android/adt-bundle-linux-x86-20131030/android-ndk-r10d/sources/cxx-stl/gnu-libstdc++/4.8/include/cstddef:
+
+/home/cobalt/Android/adt-bundle-linux-x86-20131030/android-ndk-r10d/sources/cxx-stl/gnu-libstdc++/4.8/include/cstring:
+
+/home/cobalt/Android/adt-bundle-linux-x86-20131030/android-ndk-r10d/sources/cxx-stl/gnu-libstdc++/4.8/include/cctype:
+
+/home/cobalt/Android/adt-bundle-linux-x86-20131030/android-ndk-r10d/sources/cxx-stl/gnu-libstdc++/4.8/include/string:
+
+/home/cobalt/Android/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/ptr.inl.hpp:
+
+/home/cobalt/Android/OpenCV-android-sdk/sdk/native/jni/include/opencv2/hal.hpp:
+
+/home/cobalt/Android/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/sse_utils.hpp:
+
+/home/cobalt/Android/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/traits.hpp:
+
+/home/cobalt/Android/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/matx.hpp:
+
+/home/cobalt/Android/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/types.hpp:
+
+/home/cobalt/Android/adt-bundle-linux-x86-20131030/android-ndk-r10d/sources/cxx-stl/gnu-libstdc++/4.8/include/cfloat:
+
+/home/cobalt/Android/adt-bundle-linux-x86-20131030/android-ndk-r10d/sources/cxx-stl/gnu-libstdc++/4.8/include/vector:
+
+/home/cobalt/Android/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/mat.hpp:
+
+/home/cobalt/Android/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/bufferpool.hpp:
+
+/home/cobalt/Android/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/mat.inl.hpp:
+
+/home/cobalt/Android/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/persistence.hpp:
+
+/home/cobalt/Android/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/operations.hpp:
+
+/home/cobalt/Android/adt-bundle-linux-x86-20131030/android-ndk-r10d/sources/cxx-stl/gnu-libstdc++/4.8/include/cstdio:
+
+/home/cobalt/Android/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/cvstd.inl.hpp:
+
+/home/cobalt/Android/adt-bundle-linux-x86-20131030/android-ndk-r10d/sources/cxx-stl/gnu-libstdc++/4.8/include/complex:
+
+/home/cobalt/Android/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/utility.hpp:
+
+/home/cobalt/Android/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/core_c.h:
+
+/home/cobalt/Android/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/types_c.h:
+
+/home/cobalt/Android/adt-bundle-linux-x86-20131030/android-ndk-r10d/platforms/android-8/arch-arm/usr/include/assert.h:
+
+/home/cobalt/Android/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/optim.hpp:
+
+/home/cobalt/Android/OpenCV-android-sdk/sdk/native/jni/include/opencv2/imgproc/imgproc.hpp:
+
+/home/cobalt/Android/OpenCV-android-sdk/sdk/native/jni/include/opencv2/imgproc.hpp:
+
+/home/cobalt/Android/OpenCV-android-sdk/sdk/native/jni/include/opencv2/imgproc/imgproc_c.h:
+
+/home/cobalt/Android/OpenCV-android-sdk/sdk/native/jni/include/opencv2/imgproc/types_c.h:
+
+/home/cobalt/Android/OpenCV-android-sdk/sdk/native/jni/include/opencv2/features2d/features2d.hpp:
+
+/home/cobalt/Android/OpenCV-android-sdk/sdk/native/jni/include/opencv2/features2d.hpp:
+
+/home/cobalt/Android/OpenCV-android-sdk/sdk/native/jni/include/opencv2/flann/miniflann.hpp:
+
+/home/cobalt/Android/OpenCV-android-sdk/sdk/native/jni/include/opencv2/flann/defines.h:
+
+/home/cobalt/Android/OpenCV-android-sdk/sdk/native/jni/include/opencv2/flann/config.h:
